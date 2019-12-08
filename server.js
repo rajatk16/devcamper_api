a45867a8a6dac7ebc99024fa93fbf38d5a17f66f
Rajat <rajat.sudagade@gmail.com>
2019-12-08 11:45:22 -0600

    Adding placeholder bootcamp routes

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
 // Load dotenv config
 dotenv.config({
   path: './config/config.env'
-})
+});
 
 const app = express();
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Display bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Add a new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: ` Update bootcamps ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} and listening at PORT ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} and listening at PORT ${PORT}`
+  );
 });
