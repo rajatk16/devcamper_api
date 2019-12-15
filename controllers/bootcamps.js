3f4d6e131418c2f31bf8fbe5945d529137fa3484
Rajat <rajat.sudagade@gmail.com>
2019-12-15 14:53:57 -0600

    pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   const reqQuery = { ...req.query };
 
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   removeFields.forEach(param => delete reqQuery[param]);
 
@@ -33,12 +33,37 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     const sortBy = req.query.select.split(',').join(' ');
     query = query.sort(sortBy);
   } else {
-    query.sortBy('name');
+    query.sort('name');
   }
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+  query = query.skip(startIndex).limit(limit);
 
   const bootcamps = await query;
+
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit: limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit: limit
+    };
+  }
+
   res.status(200).json({
     success: true,
+    pagination: pagination,
+    count: bootcamps.length,
     data: bootcamps
   });
 });
